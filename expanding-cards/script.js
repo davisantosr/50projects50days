97de201cb40afc0abcd338c5cc7b35d9a333aa09
davisantosr <davi-rocha2@hotmail.com>
2020-12-12 16:25:25 -0300

    Expanding Cards|Adding Extra Personal Tests

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -2,10 +2,22 @@ const panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
   panel.addEventListener('click', () => {
-    removeActiveClasses()
-    panel.classList.add('active')
+    removingActive()
+    setTimeout(() => {
+      removeActiveClasses()
+    }, 100) 
+    setTimeout(() => {
+      panel.classList.add('active')
+    }, 200)
   })
 })
+
+
+function removingActive(){
+  panels.forEach(panel => {
+    panel.classList.add('removing')
+  })
+}
 
 function removeActiveClasses() {
   panels.forEach(panel => {
